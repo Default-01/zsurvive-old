e0978a976b004e1485fd7c47a355f7d2a0da81b9
Default <j.rehmannski@gmail.com>
2022-11-16 23:02:07 +0100

    updates

--- bot.js
+++ bot.js
@@ -3,135 +3,118 @@ const PREFIX = 'z ';
 const Discord = require('discord.js-light');
 
 const client = new Discord.Client({
-    cacheGuilds: true,
-    cacheChannels: false,
-    cacheOverwrites: false,
-    cacheRoles: false,
-    cacheEmojis: false,
-    cachePresences: false,
+	cacheGuilds: true,
+	cacheChannels: false,
+	cacheOverwrites: false,
+	cacheRoles: false,
+	cacheEmojis: false,
+	cachePresences: false,
 
-    ws: {intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES']}
+	ws: { intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES'] },
 });
 
-module.exports = {client};
-
+module.exports = { client };
 
 const fs = require('fs');
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    command.name.forEach(element => {
-        client.commands.set(element, command);
-    });
-
+	const command = require(`./commands/${file}`);
+	command.name.forEach((element) => {
+		client.commands.set(element, command);
+	});
 }
 
-
 client.on('ready', () => {
+	console.log(`ZShard ${client.shard.ids[0]} online.`);
 
-    console.log(`ZShard ${client.shard.ids[0]} online.`);
+	client.user.setActivity('ZSurvive.xyz | z help');
 
-    client.user.setActivity('ZSurvive.xyz | z help');
+	setInterval(() => {
+		client.user.setActivity('ZSurvive.xyz | z help');
+	}, 1000 * 60 * 15);
 
-    setInterval(() => {
-        client.user.setActivity('ZSurvive.xyz | z help');
-    }, 1000 * 60 * 15);
+	require('./src/onReaction.js');
+	require('./src/onGuild.js');
 
-
-    require('./src/onReaction.js');
-    require('./src/onGuild.js');
-
-    if (client.shard.ids[0] === 0) {
-        require('./src/notificationsManager.js');
-        require('./src/dblapi.js');
-        require('./src/removeExpiredPatreons.js');
-    }
-
-
+	if (client.shard.ids[0] === 0) {
+		require('./src/notificationsManager.js');
+		require('./src/dblapi.js');
+		require('./src/removeExpiredPatreons.js');
+	}
 });
 
+let { getLang } = require('./functions/getLang.js');
 
-let {getLang} = require('./functions/getLang.js');
+client.on('message', async (message) => {
+	if (message.author.bot || !message.content.toLowerCase().startsWith(PREFIX)) return;
+	let args = message.content.substring(PREFIX.length).toLowerCase().split(' ');
+	const command = args.shift().toLowerCase();
+	if (!client.commands.has(command)) return;
 
+	if (message.guild === null && command !== 'notifications') return; // Private message
+	let lang = await getLang(message);
 
-client.on('message', async message => {
-
-
-    if (message.author.bot || !message.content.toLowerCase().startsWith(PREFIX)) return;
-    let args = message.content.substring(PREFIX.length).toLowerCase().split(' ');
-    const command = args.shift().toLowerCase();
-    if (!client.commands.has(command)) return;
-
-    if (message.guild === null && command !== 'notifications') return; // Private message
-    let lang = await getLang(message);
-
-
-    try {
-
-        client.commands.get(command).execute(message, args, lang);
-
-    } catch (error) {
-        sendError(error);
-        message.reply('beep boop! There was an error trying to execute that, please try again later.');
-    }
-
-
+	try {
+		client.commands.get(command).execute(message, args, lang);
+	} catch (error) {
+		sendError(error);
+		message.reply('beep boop! There was an error trying to execute that, please try again later.');
+	}
 });
 
-
-const ignore = ['Missing Permissions', 'Unknown Message', 'Missing Access', 'Cannot read property \'emoji\' of undefined', 'Cannot send messages to this user', 'AbortError',
-    'Unknown Channel', 'socket hang up', 'failed, reason: read ECONNRESET', 'Response: Internal Server Error'];
+const ignore = [
+	'Missing Permissions',
+	'Unknown Message',
+	'Missing Access',
+	"Cannot read property 'emoji' of undefined",
+	'Cannot send messages to this user',
+	'AbortError',
+	'Unknown Channel',
+	'socket hang up',
+	'failed, reason: read ECONNRESET',
+	'Response: Internal Server Error',
+];
 
 process
-    .on('unhandledRejection', async (reason) => {
-
-        let shouldSend = true;
-        ignore.forEach(element => {
-            if (reason.toString().includes(element)) shouldSend = false;
-        });
-
-        if (shouldSend) sendError(reason);
-    })
-
-
-    .on('uncaughtException', err => {
-
-        sendError(err);
-
-    });
-
+	.on('unhandledRejection', async (reason) => {
+		let shouldSend = true;
+		ignore.forEach((element) => {
+			if (reason.toString().includes(element)) shouldSend = false;
+		});
+		if (shouldSend) console.log(reason);
+		// if (shouldSend) sendError(reason);
+	})
+	.on('uncaughtException', (err) => {
+		// sendError(err);
+		console.log(err);
+	});
 
 client.login(envconfig.BotToken);
 
+// const {Webhook, MessageBuilder} = require('discord-webhook-node');
+// const errorhook = new Webhook(envconfig.ErrorWebhook);
 
-const {Webhook, MessageBuilder} = require('discord-webhook-node');
-const errorhook = new Webhook(envconfig.ErrorWebhook);
+// function sendError(err) {
 
+//     if (envconfig.Dev) console.log(err);
 
-function sendError(err) {
+//     let error = err.toString();
+//     if (err.stack) error = err.stack.toString();
 
-    if (envconfig.Dev) console.log(err);
+//     try {
 
-    let error = err.toString();
-    if (err.stack) error = err.stack.toString();
+//         if (error === '[object Response]') return; // Too many requests discord api
 
+//         const embed = new MessageBuilder()
+//             .setTitle('Beep boop... I had an error!')
+//             .setColor(14036783)
+//             .setThumbnail('https://images.emojiterra.com/google/android-nougat/512px/26a0.png')
+//             .setDescription(error);
+//         errorhook.send(embed);
 
-    try {
-
-        if (error === '[object Response]') return; // Too many requests discord api
-
-
-        const embed = new MessageBuilder()
-            .setTitle('Beep boop... I had an error!')
-            .setColor(14036783)
-            .setThumbnail('https://images.emojiterra.com/google/android-nougat/512px/26a0.png')
-            .setDescription(error);
-        errorhook.send(embed);
-
-
-    } catch (e) {
-        console.error(e + 'For some reason I could not send this to error Discord channel:\n\n', err);
-    }
-}
+//     } catch (e) {
+//         console.error(e + 'For some reason I could not send this to error Discord channel:\n\n', err);
+//     }
+// }
--- mysql/mysqlpool.js
+++ mysql/mysqlpool.js
@@ -2,51 +2,53 @@ let mysql = require('mysql');
 const envconfig = require('../envconfig.json');
 
 let sql = mysql.createPool({
-    connectionLimit: 6,
-    host: envconfig.MySQL.host,
-    user: envconfig.MySQL.user,
-    password: envconfig.MySQL.pass,
-    port: envconfig.MySQL.port,
-    supportBigNumbers: true,
-    bigNumberStrings: true,
-    charset: 'utf8mb4'
+	connectionLimit: 6,
+	database: envconfig.MySQL.database,
+	host: envconfig.MySQL.host,
+	user: envconfig.MySQL.user,
+	password: envconfig.MySQL.pass,
+	port: envconfig.MySQL.port,
+	supportBigNumbers: true,
+	bigNumberStrings: true,
+	charset: 'utf8mb4',
 });
 
 sql.getConnection((err, connection) => {
-    if (err) {
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.error('Database connection was closed.');
-        }
-        if (err.code === 'ER_CON_COUNT_ERROR') {
-            console.error('Database has too many connections.');
-        }
-        if (err.code === 'ECONNREFUSED') {
-            console.error('Database connection was refused.');
-        }
-    }
-    if (connection) connection.release();
+	if (err) {
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			console.error('Database connection was closed.');
+		}
+		if (err.code === 'ER_CON_COUNT_ERROR') {
+			console.error('Database has too many connections.');
+		}
+		if (err.code === 'ECONNREFUSED') {
+			console.error('Database connection was refused.');
+		}
+	}
+	if (connection) connection.release();
 });
-
 
 sql.query(`CREATE DATABASE IF NOT EXISTS zsurvive;`, async (err, rows) => {
-    if (err) throw err;
+	if (err) throw err;
 
-    sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.guilds (guild varchar(50) DEFAULT NULL, lang varchar(50) DEFAULT NULL);`, async (err, rows) => {
-        if (err) throw err;
-    });
-    sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.guild_count (id int NOT NULL AUTO_INCREMENT, date tinytext, amount int DEFAULT NULL, UNIQUE(id));`, async (err) => {
-        if (err) throw err;
-    });
-    sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.invites (id int NOT NULL AUTO_INCREMENT, guild text, date tinytext, joined text, UNIQUE(id));`, async (err) => {
-        if (err) throw err;
-    });
-    sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.refs (code text, main int DEFAULT '0', invite int DEFAULT '0', discord int DEFAULT '0', alias text);`, async (err) => {
-        if (err) throw err;
-    });
-    sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.users (id varchar(50) DEFAULT NULL,data mediumtext, createdAt tinytext, premium tinyint DEFAULT NULL, premiumUntil tinytext, lastPremiumCoins tinytext, userName text, energyNotifAt tinytext, voteNotifAt tinytext, votes text, totalVotes int DEFAULT '0', bossesKilled int DEFAULT '0', coinsSpent int DEFAULT '0', resourcesFarmed int DEFAULT '0');`, async (err) => {
-        if (err) throw err;
-    });
-
+	sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.guilds (guild varchar(50) DEFAULT NULL, lang varchar(50) DEFAULT NULL);`, async (err, rows) => {
+		if (err) throw err;
+	});
+	sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.guild_count (id int NOT NULL AUTO_INCREMENT, date tinytext, amount int DEFAULT NULL, UNIQUE(id));`, async (err) => {
+		if (err) throw err;
+	});
+	sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.invites (id int NOT NULL AUTO_INCREMENT, guild text, date tinytext, joined text, UNIQUE(id));`, async (err) => {
+		if (err) throw err;
+	});
+	sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.refs (code text, main int DEFAULT '0', invite int DEFAULT '0', discord int DEFAULT '0', alias text);`, async (err) => {
+		if (err) throw err;
+	});
+	sql.query(
+		`CREATE TABLE IF NOT EXISTS zsurvive.users (id varchar(50) DEFAULT NULL,data mediumtext, createdAt tinytext, premium tinyint DEFAULT NULL, premiumUntil tinytext, lastPremiumCoins tinytext, userName text, energyNotifAt tinytext, voteNotifAt tinytext, votes text, totalVotes int DEFAULT '0', bossesKilled int DEFAULT '0', coinsSpent int DEFAULT '0', resourcesFarmed int DEFAULT '0');`,
+		async (err) => {
+			if (err) throw err;
+		}
+	);
 });
 
-module.exports = {sql};
+module.exports = { sql };
