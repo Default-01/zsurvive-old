840b444c1de30128a8929cf8353588a7cecdd512
Default <j.rehmannski@gmail.com>
2022-11-16 23:13:28 +0100

    Update mysqlpool.js

--- mysql/mysqlpool.js
+++ mysql/mysqlpool.js
@@ -28,23 +28,23 @@ sql.getConnection((err, connection) => {
 	if (connection) connection.release();
 });
 
-sql.query(`CREATE DATABASE IF NOT EXISTS zsurvive;`, async (err, rows) => {
+sql.query(`CREATE DATABASE IF NOT EXISTS ${envconfig.MySQL.database};`, async (err, rows) => {
 	if (err) throw err;
 
-	sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.guilds (guild varchar(50) DEFAULT NULL, lang varchar(50) DEFAULT NULL);`, async (err, rows) => {
+	sql.query(`CREATE TABLE IF NOT EXISTS ${envconfig.MySQL.database}.guilds (guild varchar(50) DEFAULT NULL, lang varchar(50) DEFAULT NULL);`, async (err, rows) => {
 		if (err) throw err;
 	});
-	sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.guild_count (id int NOT NULL AUTO_INCREMENT, date tinytext, amount int DEFAULT NULL, UNIQUE(id));`, async (err) => {
+	sql.query(`CREATE TABLE IF NOT EXISTS ${envconfig.MySQL.database}.guild_count (id int NOT NULL AUTO_INCREMENT, date tinytext, amount int DEFAULT NULL, UNIQUE(id));`, async (err) => {
 		if (err) throw err;
 	});
-	sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.invites (id int NOT NULL AUTO_INCREMENT, guild text, date tinytext, joined text, UNIQUE(id));`, async (err) => {
+	sql.query(`CREATE TABLE IF NOT EXISTS ${envconfig.MySQL.database}.invites (id int NOT NULL AUTO_INCREMENT, guild text, date tinytext, joined text, UNIQUE(id));`, async (err) => {
 		if (err) throw err;
 	});
-	sql.query(`CREATE TABLE IF NOT EXISTS zsurvive.refs (code text, main int DEFAULT '0', invite int DEFAULT '0', discord int DEFAULT '0', alias text);`, async (err) => {
+	sql.query(`CREATE TABLE IF NOT EXISTS ${envconfig.MySQL.database}.refs (code text, main int DEFAULT '0', invite int DEFAULT '0', discord int DEFAULT '0', alias text);`, async (err) => {
 		if (err) throw err;
 	});
 	sql.query(
-		`CREATE TABLE IF NOT EXISTS zsurvive.users (id varchar(50) DEFAULT NULL,data mediumtext, createdAt tinytext, premium tinyint DEFAULT NULL, premiumUntil tinytext, lastPremiumCoins tinytext, userName text, energyNotifAt tinytext, voteNotifAt tinytext, votes text, totalVotes int DEFAULT '0', bossesKilled int DEFAULT '0', coinsSpent int DEFAULT '0', resourcesFarmed int DEFAULT '0');`,
+		`CREATE TABLE IF NOT EXISTS ${envconfig.MySQL.database}.users (id varchar(50) DEFAULT NULL,data mediumtext, createdAt tinytext, premium tinyint DEFAULT NULL, premiumUntil tinytext, lastPremiumCoins tinytext, userName text, energyNotifAt tinytext, voteNotifAt tinytext, votes text, totalVotes int DEFAULT '0', bossesKilled int DEFAULT '0', coinsSpent int DEFAULT '0', resourcesFarmed int DEFAULT '0');`,
 		async (err) => {
 			if (err) throw err;
 		}
