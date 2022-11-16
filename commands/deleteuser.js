2b653f61bb9780f5e071799b5489b78b481f78b8
Default <j.rehmannski@gmail.com>
2022-11-16 23:32:12 +0100

    updated sql queries

--- commands/deleteuser.js
+++ commands/deleteuser.js
@@ -1,28 +1,27 @@
-let {MessageEmbed} = require('discord.js');
+let { MessageEmbed } = require('discord.js');
 const config = require('../config.json');
-let {sql} = require('../mysql/mysqlpool.js');
+const envconfig = require('../envconfig.json');
+
+let { sql } = require('../mysql/mysqlpool.js');
 
 module.exports = {
-    name: ['deleteuser'],
-    async execute(message, args) {
+	name: ['deleteuser'],
+	async execute(message, args) {
+		if (!config.Admins.includes(message.author.id)) return;
+		if (!message.mentions.users.size) return message.reply('tag an user please.');
 
-        if (!config.Admins.includes(message.author.id)) return;
-        if (!message.mentions.users.size) return message.reply('tag an user please.');
+		let taggedUser = message.mentions.users.first();
 
-        let taggedUser = message.mentions.users.first();
+		sql.query(`UPDATE ${envconfig.MySQL.database}.users SET id = ? WHERE id = ? LIMIT 1;`, ['DELETED_' + taggedUser.id, taggedUser.id], async (err, result) => {
+			if (err) throw err;
 
-        sql.query(`UPDATE zsurvive.users SET id = ? WHERE id = ? LIMIT 1;`, ['DELETED_' + taggedUser.id, taggedUser.id], async (err, result) => {
-            if (err) throw err;
+			let embed = new MessageEmbed()
+				.setTitle(`Success!`)
+				.setDescription(`Database updated. Affected rows: ${result.affectedRows} (must be 1 if user existed before)`)
+				.setColor(config.EmbedsColor)
+				.setThumbnail(config.Logo);
 
-            let embed = new MessageEmbed()
-                .setTitle(`Success!`)
-                .setDescription(`Database updated. Affected rows: ${result.affectedRows} (must be 1 if user existed before)`)
-                .setColor(config.EmbedsColor)
-                .setThumbnail(config.Logo);
-
-            await message.channel.send(embed);
-
-        });
-
-    }
+			await message.channel.send(embed);
+		});
+	},
 };
